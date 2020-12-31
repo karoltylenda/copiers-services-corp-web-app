30438916f950e587d890939d3811d07624114d02
karoltylenda <karoltylenda@gmail.com>
2020-12-31 09:42:37 +0100

    add modelMapper

--- src/main/java/com/tytanisukcesu/demo/servlet/MainServlet.java
+++ src/main/java/com/tytanisukcesu/demo/servlet/MainServlet.java
@@ -2,19 +2,17 @@ package com.tytanisukcesu.demo.servlet;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-
 public class MainServlet {
 
     @GetMapping("/main/addManufacturerForm")
-    private String addManufacturerForm(){
+    private String addManufacturerForm() {
         return "addManufacturerForm";
     }
+
     @GetMapping("/main/addModelForm")
-    private String addModelForm(){
+    private String addModelForm() {
         return "addModelForm";
     }
 }
