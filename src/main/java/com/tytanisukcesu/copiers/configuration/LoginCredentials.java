bc5943b3b69ed92052fc299c0ebdc7ba5755859d
mateuszstopyra <mateuszstopyra@gmail.com>
2021-01-24 13:02:36 +0100

    adding login credentials

---
+++ src/main/java/com/tytanisukcesu/copiers/configuration/LoginCredentials.java
@@ -0,0 +1,12 @@
+package com.tytanisukcesu.copiers.configuration;
+
+import lombok.Data;
+
+@Data
+public class LoginCredentials {
+
+    private String username;
+    private String password;
+
+
+}
