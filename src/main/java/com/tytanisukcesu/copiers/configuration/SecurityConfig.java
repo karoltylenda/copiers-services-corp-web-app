f8fcfc96753f9ba407ce4618c967110cc8fd1f0b
mateuszstopyra <mateuszstopyra@gmail.com>
2021-01-24 13:12:22 +0100

    adding users and authorities schemas with first user test/test with bcrypt for password logic

---
+++ src/main/java/com/tytanisukcesu/copiers/configuration/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.tytanisukcesu.copiers.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.jdbcAuthentication()
+                    .dataSource(dataSource);
+//                    .withUser("test")
+//                    .password("{bcrypt}" + new BCryptPasswordEncoder().encode("test"))
+//                    .roles("USER");
+        }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //wylaczenie csrfa
+        http
+                .authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll() //swagger aby byl publicznie
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+
+    }
+
+
+
+}
+
+
+
+
+
