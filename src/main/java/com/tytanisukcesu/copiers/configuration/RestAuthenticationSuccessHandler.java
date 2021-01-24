7e156356f2d93daac1bdcddeab8c53c6545e2d9f
mateuszstopyra <mateuszstopyra@gmail.com>
2021-01-24 13:37:10 +0100

    adding constructor for authorization to send secret + expiration time set on 1hour

--- src/main/java/com/tytanisukcesu/copiers/configuration/RestAuthenticationSuccessHandler.java
+++ src/main/java/com/tytanisukcesu/copiers/configuration/RestAuthenticationSuccessHandler.java
@@ -1,6 +1,10 @@
 package com.tytanisukcesu.copiers.configuration;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +13,34 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final long expirationTime;
+    private final String secret;
+
+    public RestAuthenticationSuccessHandler(
+            @Value("${jwt.expirationTime}") long expirationTime,
+            @Value("${jwt.secret}") String secret) {
+        this.expirationTime = expirationTime;
+        this.secret = secret;
+    }
+
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         FilterChain chain, Authentication authentication) throws IOException, ServletException {
-        clearAuthenticationAttributes(request);
+
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
+
