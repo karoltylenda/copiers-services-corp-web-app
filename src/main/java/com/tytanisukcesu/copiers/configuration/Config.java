c778c0672e04c8f516ae8c0fd23eb098807ad89e
mateuszstopyra <mateuszstopyra@gmail.com>
2021-01-24 13:00:32 +0100

    adding apikey schema for authorization as header,adding authentification context from swagger doc and + end points schemas for tokens usage

--- src/main/java/com/tytanisukcesu/copiers/configuration/Config.java
+++ src/main/java/com/tytanisukcesu/copiers/configuration/Config.java
@@ -4,9 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableSwagger2
@@ -20,13 +29,37 @@ public class Config {
                 .select()
                 .paths(PathSelectors. regex ( "^(?!/(error).*$).*$"))
                 .paths(PathSelectors. regex ( "^(?!/(entity).*$).*$"))
-                .build();
+                .build()
+                .securitySchemes(singletonList(createSchema()))
+                .securityContexts(singletonList(createContext()));
     }
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    private SecurityContext createContext() {
+        return SecurityContext.builder()
+                .securityReferences(createRef())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> createRef() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(new SecurityReference("apiKey", authorizationScopes));
+    }
+
+    private SecurityScheme createSchema() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+
+
 
 
 }
