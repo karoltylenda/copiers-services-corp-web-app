77aca682d6016c3c62a75c66f7f7f1d498b8366c
mateuszstopyra <mateuszstopyra@gmail.com>
2021-03-27 10:23:02 +0100

    adding new model servlet methods to be comparable with endpoint /models

--- src/main/java/com/tytanisukcesu/copiers/servlet/ModelServlet.java
+++ src/main/java/com/tytanisukcesu/copiers/servlet/ModelServlet.java
@@ -8,7 +8,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,13 +27,26 @@ public class ModelServlet {
         return "pages/models";
     }
 
-    private ModelDto convertToDto(Model model) {
+    @PostMapping
+    public RedirectView save(ModelDto modelDto){
+        modelService.save(convertToEntity(modelDto));
+        return new RedirectView("/models");
+    }
+
+    @PostMapping
+    public RedirectView update(Long id,ModelDto modelDto){
+        modelService.update(id,convertToEntity(modelDto));
+        return new RedirectView("/models");
+    }
+
+
+    private ModelDto convertToDto(com.tytanisukcesu.copiers.entity.Model model) {
         ModelDto modelDto = modelMapper.map(model, ModelDto.class);
         return modelDto;
     }
 
-    private Model convertToEntity(ModelDto modelDto) {
-        Model model = modelMapper.map(modelDto, Model.class);
+    private com.tytanisukcesu.copiers.entity.Model convertToEntity(ModelDto modelDto) {
+        com.tytanisukcesu.copiers.entity.Model model = modelMapper.map(modelDto, com.tytanisukcesu.copiers.entity.Model.class);
         return model;
     }
 
