7bd5556eac64ed3692916e29f47687ea07d5328b
karol <karoltylenda@gmail.com>
2021-04-03 17:51:00 +0200

    update contracts.html

--- src/main/java/com/tytanisukcesu/copiers/servlet/ContractServlet.java
+++ src/main/java/com/tytanisukcesu/copiers/servlet/ContractServlet.java
@@ -2,6 +2,7 @@ package com.tytanisukcesu.copiers.servlet;
 
 
 import com.tytanisukcesu.copiers.service.ContractService;
+import com.tytanisukcesu.copiers.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ContractServlet {
 
     private final ContractService contractService;
+    private final CustomerService customerService;
 
     @GetMapping
     public String findAll(Model model){
         model.addAttribute("contracts", contractService.findAll());
+        model.addAttribute("customers", customerService.findAll());
         return ("pages/contracts");
     }
 }
