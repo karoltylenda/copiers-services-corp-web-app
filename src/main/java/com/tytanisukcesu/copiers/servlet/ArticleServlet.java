b35d00586df69e664dda322ce7f608ddf529d088
mateuszstopyra <mateuszstopyra@gmail.com>
2021-04-10 13:09:44 +0200

    adding corrections to add new article modal

--- src/main/java/com/tytanisukcesu/copiers/servlet/ArticleServlet.java
+++ src/main/java/com/tytanisukcesu/copiers/servlet/ArticleServlet.java
@@ -1,7 +1,6 @@
 package com.tytanisukcesu.copiers.servlet;
 
 import com.tytanisukcesu.copiers.dto.ArticleDto;
-import com.tytanisukcesu.copiers.dto.ModelDto;
 import com.tytanisukcesu.copiers.entity.Article;
 import com.tytanisukcesu.copiers.service.ArticleService;
 import com.tytanisukcesu.copiers.service.ManufacturerService;
@@ -10,11 +9,14 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/articles")
@@ -29,37 +31,40 @@ public class ArticleServlet {
     @GetMapping
     public String findAll(Model model) {
         model.addAttribute("articles", articleService.findAll());
-        model.addAttribute("manufacturers",manufacturerService.findAll());
-        model.addAttribute("models",modelService.findAll());
+        model.addAttribute("manufacturers", manufacturerService.findAll());
+        model.addAttribute("models", modelService.findAll());
         return "pages/articles";
     }
 
+    //wyrzuc do oddzielnej metody - najlepiej w serwisie
     @PostMapping
-    public RedirectView save(ArticleDto articleDto,ArrayList<Integer> modelsList){
-        articleService.save(convertToEntity(articleDto));
+    public RedirectView save(ArticleDto articleDto, Integer[] modelsArray) {
+        Set<com.tytanisukcesu.copiers.entity.Model> models = new HashSet<>();
+        for (Integer integer : modelsArray) {
+            Long modelId = Long.valueOf(integer);
+            models.add(modelService.findById(modelId));
+        }
+        Article article = convertToEntity(articleDto);
+        article.setModels(models);
+        articleService.save(article);
         return new RedirectView("/articles");
     }
 
     @GetMapping(value = "/")
-    public String findByCustomer(@RequestParam Long id, Model model){
+    public String findByCustomer(@RequestParam Long id, Model model) {
         model.addAttribute("models", articleService.findById(id).getModels());
         return "pages/articles";
     }
 
-    private ArticleDto convertToDto(Article article){
+    private ArticleDto convertToDto(Article article) {
         ArticleDto articleDto = modelMapper.map(article, ArticleDto.class);
         return articleDto;
     }
 
-    private Article convertToEntity(ArticleDto articleDto){
+    private Article convertToEntity(ArticleDto articleDto) {
         Article article = modelMapper.map(articleDto, Article.class);
         return article;
     }
-
-
-
-
-
 
 
 }
