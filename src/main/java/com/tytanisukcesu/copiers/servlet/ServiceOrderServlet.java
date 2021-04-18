f664876ad48213417929b34a1f93bda687f6711f
mateuszstopyra <mateuszstopyra@gmail.com>
2021-04-18 20:23:57 +0200

    adding service order servlet and html

--- src/main/java/com/tytanisukcesu/copiers/servlet/ServiceOrderServlet.java
+++ src/main/java/com/tytanisukcesu/copiers/servlet/ServiceOrderServlet.java
@@ -1,8 +1,11 @@
 package com.tytanisukcesu.copiers.servlet;
 
 
+import com.tytanisukcesu.copiers.dto.ServiceOrderDto;
+import com.tytanisukcesu.copiers.entity.ServiceOrder;
 import com.tytanisukcesu.copiers.service.ServiceOrderService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +17,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ServiceOrderServlet {
 
     private final ServiceOrderService serviceOrderService;
+    private final ModelMapper modelMapper;
 
     @GetMapping
     public String findAll(final Model model){
         model.addAttribute("serviceOrders", serviceOrderService.findAll());
         return ("pages/serviceOrders");
     }
+
+    private ServiceOrderDto convertToDto(ServiceOrder serviceOrder){
+        ServiceOrderDto serviceOrderDto = modelMapper.map(serviceOrder, ServiceOrderDto.class);
+        return serviceOrderDto;
+    }
+
+    private ServiceOrder convertToEntity(ServiceOrderDto serviceOrderDto){
+        ServiceOrder serviceOrder = modelMapper.map(serviceOrderDto,ServiceOrder.class);
+        return serviceOrder;
+    }
+
+
 }
