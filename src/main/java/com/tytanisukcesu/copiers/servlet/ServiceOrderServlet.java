f9ffb6b5e6ef4da4555d38b104729ec21282753d
mateuszstopyra <mateuszstopyra@gmail.com>
2021-04-22 20:24:37 +0200

    adding block to the SO modal

--- src/main/java/com/tytanisukcesu/copiers/servlet/ServiceOrderServlet.java
+++ src/main/java/com/tytanisukcesu/copiers/servlet/ServiceOrderServlet.java
@@ -3,13 +3,16 @@ package com.tytanisukcesu.copiers.servlet;
 
 import com.tytanisukcesu.copiers.dto.ServiceOrderDto;
 import com.tytanisukcesu.copiers.entity.ServiceOrder;
+import com.tytanisukcesu.copiers.service.DeviceService;
 import com.tytanisukcesu.copiers.service.ServiceOrderService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,12 +20,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ServiceOrderServlet {
 
     private final ServiceOrderService serviceOrderService;
+    private final DeviceService deviceService;
     private final ModelMapper modelMapper;
 
     @GetMapping
-    public String findAll(final Model model){
+    public String findAll(Model model){
         model.addAttribute("serviceOrders", serviceOrderService.findAll());
+        model.addAttribute("devices", deviceService.findAll());
         return ("pages/serviceOrders");
+    }
+
+    @PostMapping
+    public RedirectView save(ServiceOrderDto serviceOrderDto){
+        serviceOrderService.save(convertToEntity(serviceOrderDto));
+        return new RedirectView("/serviceOrders");
     }
 
     private ServiceOrderDto convertToDto(ServiceOrder serviceOrder){
