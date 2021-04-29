db379ba045c5a6fd6b3d37d5f6497f774b792740
mateuszstopyra <mateuszstopyra@gmail.com>
2021-04-29 13:30:50 +0200

    HOME UPDATES - adding 3x total cards

--- src/main/java/com/tytanisukcesu/copiers/servlet/HomeServlet.java
+++ src/main/java/com/tytanisukcesu/copiers/servlet/HomeServlet.java
@@ -1,13 +1,25 @@
 package com.tytanisukcesu.copiers.servlet;
 
+import com.tytanisukcesu.copiers.service.ContractService;
+import com.tytanisukcesu.copiers.service.CustomerService;
+import com.tytanisukcesu.copiers.service.ServiceOrderService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HomeServlet {
+
+    private final CustomerService customerService;
+    private final ContractService contractService;
+    private final ServiceOrderService serviceOrderService;
+
 
     @GetMapping
     public RedirectView main() {
@@ -15,7 +27,10 @@ public class HomeServlet {
     }
 
     @GetMapping(value = "/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("customers", customerService.findAll());
+        model.addAttribute("contracts", contractService.findAll());
+        model.addAttribute("serviceOrders", serviceOrderService.findAll());
         return ("pages/dashboard");
     }
 }
